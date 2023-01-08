0003635eb67f3cc3d6371e33ff45a8a2e28bb16b
Guillaume Alzieu <guillaume.alzieu@univ-grenoble-alpes.fr>
2023-01-08 19:44:21 +0100

    Finish mobile version + try some colors

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,28 @@
+var currentScroll = null
+
 document.addEventListener('DOMContentLoaded', function() {
+
+    // This adds a listener to perform the menu toggling/untoggling
+    // It basically displays the navigation menu and hides all the page content
+    // so that the menu takes all the available space.
     const menuToggle = document.getElementById('mobile-menu-toggle')
     var menuToggled = false
 
     if (menuToggle.style.display != 'none') {
         const menu = document.getElementById('mobile-sidebar-content')
+        const contentWrapper = document.getElementById('content-wrapper')
         menuToggle.addEventListener('click', function () {
             if (!menuToggled) {
+                currentScroll = document.documentElement.scrollTop || document.body.scrollTop
                 menu.classList.toggle('mobile-sidebar-content-active')
+                contentWrapper.style.display = 'none'
                 menuToggled = true
             }
             else {
+                contentWrapper.style.display = 'block'
                 menu.classList.remove('mobile-sidebar-content-active')
+                document.documentElement.scrollTop = document.body.scrollTop = currentScroll
+                currentScroll = null
                 menuToggled = false
             }
         })
